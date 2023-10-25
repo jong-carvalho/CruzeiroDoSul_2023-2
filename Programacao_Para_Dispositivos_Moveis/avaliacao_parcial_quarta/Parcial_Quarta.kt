b987f1df06e4635a5c96bda65d0e98ce52ed3f52
Jonatas Carvalho <jonatas.ccp@gmail.com>
2023-10-25 19:22:35 -0300

    adding avaliacao parcial quarta

---
+++ Programacao_Para_Dispositivos_Moveis/avaliacao_parcial_quarta/Parcial_Quarta.kt
@@ -0,0 +1,13 @@
+
+fun main() {
+
+
+    val nomes = arrayOf("Maria Silva", "João Santos", "Ana Pereira", "Pedro Oliveira", "Mariana Sousa", "Lucas Fernandes", "Carolina Almeida", "Gustavo Barbosa", "Isabela Castro", "Rafael Ferreira", "Beatriz Gomes", "Daniel Rodrigues", "Juliana Costa", "Matheus Martins", "Larissa Rocha", "Julia Mancini")
+    val notas = arrayOf(7, 8, 3, 7, 2, 10, 9, 8, 6, 7, 4, 6, 8, 9, 9, 1)
+    val rgms = arrayOf(3345, 3362, 3331, 3372, 3389, 3344, 3367, 3390, 3361, 3322, 3342, 3351, 3353, 3398, 3309, 3304)
+
+   
+
+}
+
+
